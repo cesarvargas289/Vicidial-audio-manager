ce943195209b64a84a9da7bcbacc9590975ee258
César Vargas <cesarvargas289@gmail.com>
2018-08-15 15:17:41 -0700

    User migration

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,14 +19,14 @@ class UserTableSeeder extends Seeder
         $user = new User();
         $user->name = 'User';
         $user->email = 'user@dish.com.mx';
-        $user->password = ('Dish2018');
+        $user->password = bcrypt('Dish2018');
         $user->save();
         $user->roles()->attach($role_user);
 
         $user = new User();
         $user->name = 'Admin';
-        $user->email = 'admin@example.com';
-        $user->password = ('Dish2018');
+        $user->email = 'admin@dish.com.mx';
+        $user->password = bcrypt('Dish2018');
         $user->save();
         $user->roles()->attach($role_admin);
     }
